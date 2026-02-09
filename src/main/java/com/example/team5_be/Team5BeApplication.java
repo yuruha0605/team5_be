efdabb5546ebe8049fbb3277e8160f1fde648e4f
박명환 <jackpark5872@yonsei.ac.kr>
2026-02-09 09:58:24 +0900

    1차 등록본

---
+++ src/main/java/com/example/team5_be/Team5BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.team5_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5BeApplication.class, args);
+	}
+
+}
