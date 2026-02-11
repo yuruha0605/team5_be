397df3cd71c208b1f3c305cbffc6fe75fe06f8a8
박명환 <jackpark5872@yonsei.ac.kr>
2026-02-11 11:14:47 +0900

    2/11 11:15 MissionCtrl - API 명세서 부분 추가

--- src/main/java/com/example/team5_be/mission/ctrl/MissionController.java
+++ src/main/java/com/example/team5_be/mission/ctrl/MissionController.java
@@ -13,22 +13,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.team5_be.comment.domain.dto.CommentResponseDTO;
 import com.example.team5_be.mission.domain.dto.MissionRequestDTO;
 import com.example.team5_be.mission.domain.dto.MissionResponseDTO;
 import com.example.team5_be.mission.service.MissionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/missions")
 @RequiredArgsConstructor
-@Tag(name = "Mission", description = "미션 생성, 조회, 수정, 삭제 관련 명세서")
+@Tag(name = "Mission", description = "미션 관련 명세서")
 public class MissionController {
 
     private final MissionService missionService ;
 
     // 미션 생성
+    @ApiResponses(
+        {
+            @ApiResponse(
+                responseCode="201", 
+                description="미션 생성(등록) 성공",
+                content = @Content(schema = @Schema(implementation = MissionResponseDTO.class))),
+            @ApiResponse(responseCode="400" , description="미션 생성(등록) 실패")
+        }
+    )
+    @Operation(
+        summary = "미션 생성(등록)",
+        description = "미션 생성(등록)을 수행합니다."
+    )
     @PostMapping("/register")
     public ResponseEntity<MissionResponseDTO> create(@RequestBody MissionRequestDTO request) {
         System.out.println(">>>> mission ctrl create check");
@@ -40,6 +59,19 @@ public class MissionController {
 
 
     // mission ID로 미션 단건 조회
+    @ApiResponses(
+        {
+            @ApiResponse(
+                responseCode="200", 
+                description="미션 조회 성공",
+                content = @Content(schema = @Schema(implementation = MissionResponseDTO.class))),
+            @ApiResponse(responseCode="404" , description="존재하지 않는 미션")
+        }
+    )
+    @Operation(
+        summary = "미션 단건 조회",
+        description = "미션 아이디를 이용한 조회"
+    )
     @GetMapping("/read/{missionId}") 
     public ResponseEntity<MissionResponseDTO> read(@PathVariable("missionId") Integer missionId) {
         System.out.println(">>>> mission ctrl read check");
@@ -53,6 +85,20 @@ public class MissionController {
 
 
     // 모든 미션 조회
+
+    @ApiResponses(
+        {
+            @ApiResponse(
+                responseCode="200", 
+                description="미션 목록 조회 성공",
+                content = @Content(schema = @Schema(implementation = MissionResponseDTO.class))),
+            @ApiResponse(responseCode="404" , description="존재하지 않는 유저")
+        }
+    )
+    @Operation(
+        summary = "유저의 개인 미션 목록 조회",
+        description = "유저 아이디를 이용한 미션 목록 조회"
+    )
     @GetMapping("/list/{userId}")
     public ResponseEntity<List<MissionResponseDTO>> list(@PathVariable("userId") String userId) {
         System.out.println(">>>> mission ctrl list check");
@@ -62,6 +108,19 @@ public class MissionController {
 
 
     // 미션 수정
+    @ApiResponses(
+        {
+            @ApiResponse(
+                responseCode="200", 
+                description="미션 수정 성공",
+                content = @Content(schema = @Schema(implementation = MissionResponseDTO.class))),
+            @ApiResponse(responseCode="400" , description="미션 수정 실패")
+        }
+    )
+    @Operation(
+        summary = "미션 수정",
+        description = "미션 아이디를 이용한 수정"
+    )
     @PutMapping("/update/{missionId}")
     public ResponseEntity<MissionResponseDTO> update(@PathVariable("missionId") Integer missionId, @RequestBody MissionRequestDTO request) {
         System.out.println(">>>> mission ctrl update check");
@@ -74,6 +133,18 @@ public class MissionController {
 
 
     // 미션 삭제
+    @ApiResponses(
+        {
+            @ApiResponse(
+                responseCode="204", 
+                description="미션 삭제 성공"),
+            @ApiResponse(responseCode="404" , description="미션 삭제 실패")
+        }
+    )
+    @Operation(
+        summary = "미션 삭제",
+        description = "미션 아이디를 이용한 삭제"
+    )
     @DeleteMapping("/delete/{missionId}") 
     public ResponseEntity<Boolean> delete(@PathVariable("missionId") Integer missionId) {
         System.out.println(">>>> mission ctrl delete check");
