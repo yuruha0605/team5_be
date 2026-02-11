f926ef5fd8aedd8e1d5dd14a13754699976bfe2b
박명환 <jackpark5872@yonsei.ac.kr>
2026-02-11 18:13:02 +0900

    2/11 18:13 박명환

--- src/main/java/com/example/team5_be/dashboard/domain/entity/DashBoardEntity.java
+++ src/main/java/com/example/team5_be/dashboard/domain/entity/DashBoardEntity.java
@@ -1,27 +1,27 @@
-package com.example.team5_be.dashboard.domain.entity;
+// package com.example.team5_be.dashboard.domain.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "dashboard_view")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class DashBoardEntity {
+// @Entity
+// @Table(name = "dashboard_view")
+// @Getter
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class DashBoardEntity {
 
-    @Id
-    @Column(name = "user_id")
-    private String userId;
+//     @Id
+//     @Column(name = "user_id")
+//     private String userId;
 
-    @Column(name = "user_name")
-    private String userName;
+//     @Column(name = "user_name")
+//     private String userName;
 
-    @Column(name = "total_score")
-    private Long totalScore;
-}
+//     @Column(name = "total_score")
+//     private Long totalScore;
+// }
